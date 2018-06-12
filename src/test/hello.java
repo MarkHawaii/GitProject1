c5f23fe2d7c9af8c7d90fc6b5dd713bf3c10e8b4
mekangurbanov88 <mekangurbanov88@gmail.com>
2018-06-11 19:23:38 -0700

    Second

--- src/test/hello.java
+++ src/test/hello.java
@@ -3,5 +3,6 @@ package test;
 public class hello {
 public static void main(String[] args) {
 	System.out.println("Hello GitHub Project");
-}
+	System.out.println("What do you think about JAVA");
+	}
 }
