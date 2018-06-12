638d1b314c04020004c39049bfc715d351209301
mekangurbanov88 <mekangurbanov88@gmail.com>
2018-06-11 19:26:03 -0700

    it is Third

--- src/test/hello.java
+++ src/test/hello.java
@@ -4,5 +4,6 @@ public class hello {
 public static void main(String[] args) {
 	System.out.println("Hello GitHub Project");
 	System.out.println("What do you think about JAVA");
+	System.out.println("It is third line");
 	}
 }
